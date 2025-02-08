6bd1b919aad4d52645267854d939e737eb33ff89
Joe <40008990+joxpUB@users.noreply.github.com>
2025-02-08 16:51:12 -0500

    Update Intune_Remediation_IdlePowerSetting.ps1
    SCCM -> Intune transition needed a bit more exit codes for consistency's sake. Covered all cases, regardless of how they'd happen.

--- Intune_Remediation_IdlePowerSetting.ps1
+++ Intune_Remediation_IdlePowerSetting.ps1
@@ -1,4 +1,4 @@
-﻿# Intune Remediation Script for Intune_Detection_IdlePowerSetting.ps1
+# Intune Remediation Script for Intune_Detection_IdlePowerSaving.ps1
 # Sets the Idle Power Saving property to Disabled, due to an issue with Cisco AnyConnect VPN dropping through docking monitors. This is especially prevalent/necessary with everyone who uses Cisco Finesse. Specifically created for ticket# 00615097
 # Created: jfrocco 3/9/2023
 # Turned into an Intune Remediation Script 2/8/2025, pretend this is a signed PS script. Do not run with logged-on credentials, Admin is necessary.
@@ -11,9 +11,17 @@ try{
     # If the EnableExtraPowerSaving (AKA Idle Power Saving) keyword exists, disable it but do not restart the network adapter. Else, do nothing!
     if($IdlePowerSaving -ne "0"){
         Set-NetAdapterAdvancedProperty -RegistryKeyword "EnableExtraPowerSaving" -RegistryValue "0" -NoRestart # This change will not take effect until the network adapter restarts, so as not to distrupt startup processes.
+
+        Write-Host "Idle Power Saving has been disabled."
+        exit 0
     }
-    exit 0
+    # It shouldn't be able to get to this state
+    else{
+        Write-Host "Idle Power Saving is already disabled."
+        exit 0
+    }
 }
+
 catch{
     # EnableExtraPowerSaving likely does not exist if it gets here! Or something went wrong.
     Write-Error "($_.Exception.Message)"
