a1cb20ea2cc95d6409872768d1ea7ddbb0bcdeed
Joe <40008990+joxpUB@users.noreply.github.com>
2025-02-08 15:43:55 -0500

    Add files via upload
    Intune Remediation Script examples, adapted from a previous SCCM script.

---
+++ Intune_Detection_IdlePowerSetting.ps1
@@ -0,0 +1,22 @@
+﻿# Intune Detection Script for Intune_Remediation_IdlePowerSaving.ps1
+# Detects the Idle Power Saving property on all network adapters, if it exists. Created due to an issue with Cisco AnyConnect VPN dropping through docking monitors. This is especially prevalent/necessary with everyone who uses Cisco Finesse. Specifically created for ticket# 00615097
+# Created: jfrocco 2/8/2025, pretend this is a signed PS script.
+
+try{
+
+    $IdlePowerSaving = (Get-NetAdapterAdvancedProperty -RegistryKeyword EnableExtraPowerSaving -ErrorAction Stop).RegistryValue
+
+    if($IdlePowerSaving -ne "0"){
+        Write-Host "Idle Power Saving is enabled, this will be remediated."
+        exit 1
+    }
+    else{
+        Write-Host "Idle Power Saving is NOT enabled, no remediation necessary."
+        exit 0
+    }
+}
+
+catch{
+    Write-Host "EnableExtraPowerSaving AKA Idle Power Saving likely does not exist on this device, no remediation necessary."
+    exit 0
+}
---
+++ Intune_Remediation_IdlePowerSetting.ps1
@@ -0,0 +1,21 @@
+﻿# Intune Remediation Script for Intune_Detection_IdlePowerSetting.ps1
+# Sets the Idle Power Saving property to Disabled, due to an issue with Cisco AnyConnect VPN dropping through docking monitors. This is especially prevalent/necessary with everyone who uses Cisco Finesse. Specifically created for ticket# 00615097
+# Created: jfrocco 3/9/2023
+# Turned into an Intune Remediation Script 2/8/2025, pretend this is a signed PS script. Do not run with logged-on credentials, Admin is necessary.
+
+
+try{
+
+    $IdlePowerSaving = (Get-NetAdapterAdvancedProperty -RegistryKeyword EnableExtraPowerSaving -ErrorAction Stop).RegistryValue
+
+    # If the EnableExtraPowerSaving (AKA Idle Power Saving) keyword exists, disable it but do not restart the network adapter. Else, do nothing!
+    if($IdlePowerSaving -ne "0"){
+        Set-NetAdapterAdvancedProperty -RegistryKeyword "EnableExtraPowerSaving" -RegistryValue "0" -NoRestart # This change will not take effect until the network adapter restarts, so as not to distrupt startup processes.
+    }
+    exit 0
+}
+catch{
+    # EnableExtraPowerSaving likely does not exist if it gets here!
+    Write-Error "($_.Exception.Message)"
+    exit 0
+}
